de9c2b914abddb4d36193a83eec33748b2668c2a
Mayank Chahan <996mayank@gmail.com>
2025-07-26 16:45:13 +0530

    folder structure

--- setup.py
+++ setup.py
@@ -1,1 +1,8 @@
-print('hello genAi')
+from setuptools import setup, find_packages
+
+setup(
+    name="document_portal",
+    author="Mayank Chauhan",
+    version="0.1",
+    packages=find_packages(),
+)
